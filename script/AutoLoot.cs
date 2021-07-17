d4fd04ba1cf30ba216ecf48d90930942bba47ec2
elmagnificoco <elmagnificogg@gmail.com>
2021-07-18 00:01:02 +0800

    slower click buy and fix always in dungeon

--- script/AutoLoot.cs
+++ script/AutoLoot.cs
@@ -73,7 +73,7 @@ namespace BeastAutoRun.script
                         option.Click(990, 150);
                         break;
                 }
-                option.Wait(50);
+                option.Wait(100);
             }
             Debug.WriteLine("buy stop");
         }
@@ -86,16 +86,20 @@ namespace BeastAutoRun.script
 
         public void start()
         {
-            if (!ready())
+             if (!ready())
                 return;
 
             run = true;
             int onlyHome = 0;
             int goon = 0;
+            int defeat = 0;
+            int arena_time = 0;
             req_stop = false;
+            int loop_time = 0;
 
             while (!req_stop)
             {
+
                 // get treasure at home
                 if (option.RegnizeColor(267, 485, "BFC4CC")
                     && option.RegnizeColor(322, 485, "BFC4CC")
@@ -132,16 +136,6 @@ namespace BeastAutoRun.script
                     option.Wait(2000);
                 }
 
-                //// run dungeon close dialogue
-                //if (option.RegnizeColor(335, 300, "BC752F")
-                //    && option.RegnizeColor(626, 330, "BC752F")
-                //    && option.RegnizeColor(478, 300, "BC752F"))
-                //{
-                //    Debug.WriteLine("run dungeon close dialogue");
-                //    option.Click(626, 330);
-                //    option.Wait(2000);
-                //}
-
                 // get treasure in dungeon
                 if (option.RegnizeColor(585, 485, "A1A5AC")
                     && option.RegnizeColor(637, 485, "A1A5AC")
@@ -155,18 +149,25 @@ namespace BeastAutoRun.script
                     option.Wait(2000);
                 }
 
-                // enter dungeon
+                // enter arena
                 if (option.RegnizeColor(807, 652, "BECDE5")
                     && option.RegnizeColor(825, 627, "BECDE5"))
                 {
-                    Debug.WriteLine("enter arena");
-                    option.Click(807, 652);
-                    option.Wait(2000);
+                    // more than about 100s from last arena
+                    if(loop_time -arena_time > 1000)
+                    {
+                        Debug.WriteLine("enter arena");
+                        option.Click(807, 652);
+                        option.Wait(2000);
+                        arena_time = loop_time;
+                    }
                 }
 
-                if (option.RegnizeColor(643, 521, "BFC4CC"))
+                // get arena treasure
+                if (option.RegnizeColor(643, 521, "BFC4CC")
+                    && option.RegnizeColor(340, 334, "FFCE0A"))
                 {
-                    Debug.WriteLine("enter arena");
+                    Debug.WriteLine("get arena treasure");
                     for (int i = 0; i < 20; i++)
                     {
                         option.Click(643, 521);
@@ -198,16 +199,28 @@ namespace BeastAutoRun.script
                     && option.RegnizeColor(610, 292, "E52023")
                     && option.RegnizeColor(893, 313, "E52023"))
                 {
-                    Debug.WriteLine("get treasure when defeat");
-                    for (int i = 0; i < 20; i++)
+                    // if has chance to change opponent
+                    if(option.RegnizeColor(1155, 641, "FF5C54")&&defeat<2)
                     {
-                        option.Click(637, 500);
+                        Debug.WriteLine("change opponent try again");
+                        defeat++;
+                        option.Click(1155, 641);
+                        option.Wait(2000);
+                    }
+                    else
+                    {
+                        Debug.WriteLine("get treasure when defeat");
+                        for (int i = 0; i < 20; i++)
+                        {
+                            option.Click(637, 500);
+                        }
+
+                        // go home
+                        Debug.WriteLine("go home");
+                        option.Click(91, 658);
+                        option.Wait(2000);
                     }
 
-                    // go home
-                    Debug.WriteLine("go home");
-                    option.Click(91, 658);
-                    option.Wait(2000);
                 }
 
                 // close dialogue when run
@@ -257,6 +270,7 @@ namespace BeastAutoRun.script
                         Debug.WriteLine("go home");
                         option.Click(91, 658);
                         option.Wait(2000);
+                        defeat = 0;
                     }
 
                 }else
@@ -266,6 +280,7 @@ namespace BeastAutoRun.script
 
                 //option.Click(637, 357);
                 option.Wait(100);
+                loop_time++;
             }
             Debug.WriteLine("AutoLoot stop");
 
